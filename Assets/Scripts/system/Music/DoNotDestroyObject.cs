98fe0ec0a3ed525ceb090158caed8ada71c7deff
Pavlo Ieremeiew <peremuv3@gmail.com>
2023-09-24 01:57:39 +0300

    effects

--- Assets/Scripts/system/Music/DoNotDestroyObject.cs
+++ Assets/Scripts/system/Music/DoNotDestroyObject.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class DoNotDestroyObject : MonoBehaviour
 {
+    [SerializeField] private string _objName;
     private void Awake()
     {
-        GameObject[] musicObject = GameObject.FindGameObjectsWithTag("MusicObject");
+        GameObject[] musicObject = GameObject.FindGameObjectsWithTag(_objName);
         if (musicObject.Length > 1)
         {
             Destroy(this.gameObject);
